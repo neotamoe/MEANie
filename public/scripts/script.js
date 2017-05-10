8f88c2d4eb086baaeb2725f3d99c7890a15797f4
Neota Moe <neota.moe@gmail.com>
2017-05-10 14:47:50 -0500

    script formatting fixed

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,28 +1,31 @@
 var myApp=angular.module( 'myApp', [] );
+
 myApp.controller( 'WhereMyPeeps', [ '$http', function( $http ){
-var vm = this;
-vm.addRecord = function(){
-var objectToSend ={
-name= vm.nameIn,
-location= vm.locationIn,
-};
-$http({
-method: 'POST',
-url: '/testPost',
-data: objectToSend
-});
-vm.nameIn ='';
-vm.locationIn='';
-};
-vm.getRecords = function(){
-$.http({
-method: 'GET',
-url: '/getRecords',
-}).then( function( response ){
-vm.allTheRecords = response;
-console.log( vm.allTheRecords );
-}), function myError( response ){
-console.log( response.statusText );
-};
-};
+  var vm = this;
+
+  vm.addRecord = function(){
+    var objectToSend ={
+      name= vm.nameIn,
+      location= vm.locationIn,
+    };
+    $http({
+      method: 'POST',
+      url: '/testPost',
+      data: objectToSend
+    });
+    vm.nameIn ='';
+    vm.locationIn='';
+  };
+
+  vm.getRecords = function(){
+    $.http({
+      method: 'GET',
+      url: '/getRecords',
+    }).then( function( response ){
+      vm.allTheRecords = response;
+      console.log( vm.allTheRecords );
+    }), function myError( response ){
+      console.log( response.statusText );
+    };
+  };
 }]);
